6616c7885f1cb8015a5855a37ce4d7922adfa4a1
FranParraH <156036503+FranParraH@users.noreply.github.com>
2024-01-13 01:42:06 +0000

    actualizacion

---
+++ src/js/component/card.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+const Card = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch('https://swapi.dev/api/people/');
+        if (!response.ok) {
+          throw new Error('Failed to fetch characters');
+        }
+        const data = await response.json();
+        setCharacters(data.results);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
+  const getCharacterImage = (id) => {
+    return `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`;
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        {characters.map((character, index) => (
+          <div key={index} className="col-md-4 mt-5">
+            <div className="card mb-3" style={{ width: '100%', height: '100%' }}>
+              <img
+                src={getCharacterImage(index + 1)}
+                className="card-img-top"
+                alt={`Character ${index + 1}`}
+                style={{ width: '100%', height: '100%' }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{character.name}</h5>
+                <p className="card-text">Height: {character.height}</p>
+                <p className="card-text">Gender: {character.gender}</p>
+                {/* Add more details if necessary */}
+                <a href="#" className="btn btn-success">
+			    Entrar a Ficha 
+		        </a>
+                <a href="#" className="btn btn-success">
+			    Favoritos
+		        </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,17 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+
 import "../../styles/home.css";
+import Card from "../component/card";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+		<h1>Blog Starwars
+		</h1>
+		<div>
+			<Card/>
+		</div>
 		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
+			boton 
 		</a>
 	</div>
 );
